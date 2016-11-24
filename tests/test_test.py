5868ee2cd8a67f9121d5c1aa6cb1efa16e170b92
Hayden <hayden.field@devinit.org>
2016-11-24 16:42:30 +0000

    Explore method of locating text in page
    This explores a method of locating text in a page.
    It utilises an xpath and a string to find in that location.
    This requires that all URLs in a class can pass tests in the class

---
+++ tests/test_test.py
@@ -0,0 +1,24 @@
+import pytest
+from web_test_base import *
+
+"""
+A class to test new features without running all of the tests.
+
+Usage:
+    py.test tests/test_test.py -rsx
+"""
+class TestTest(WebTestBase):
+    urls_to_get = [
+        "http://aidtransparency.net/"
+    ]
+    text_to_find = [
+        ("information", '//*[@id="home-strapline"]/h1')
+    ]
+
+    def test_locate_text(self, loaded_request, text_to_find):
+        """
+        Tests that each page contains lthe specified text at the required location.
+        """
+        result = self._get_text_from_xpath(loaded_request, text_to_find[1])
+
+        assert self._substring_in_list(text_to_find[0], result)
--- tests/web_test_base.py
+++ tests/web_test_base.py
@@ -25,12 +25,22 @@ class WebTestBase:
 
     def _get_links_from_page(self, request):
         """
-        Locates the location of all <a href="...">...</a> tags on the page associated
-        with the provided request.
+        Locates the location of all <a href="...">...</a> tags on the page
+        associated with the provided request.
         Returns a list of strings containing the linked URLs
             ie. the contents of the `href` attribute
         """
-        return  self._locate_xpath_result(request, "//a[@href]/@href")
+        return self._locate_xpath_result(request, "//a[@href]/@href")
+
+    def _get_text_from_xpath(self, request, xpath):
+        """
+        Locates the notes within the HTML at the specific xpath.
+        Returns a list of strings containing the contents of these nodes.
+        """
+        return self._locate_xpath_result(request, xpath + "/text()")
+
+    def _substring_in_list(self, substr_to_find, list_to_search):
+        return len([s for s in list_to_search if substr_to_find in s]) > 0
 
     @classmethod
     def setup_class(cls):
@@ -49,6 +59,8 @@ class WebTestBase:
         """
         if 'urls_to_get' in metafunc.fixturenames:
             metafunc.parametrize("urls_to_get", cls.urls_to_get)
+        if 'text_to_find' in metafunc.fixturenames:
+            metafunc.parametrize("text_to_find", cls.text_to_find)
 
     @pytest.fixture
     def loaded_request(cls, urls_to_get):
