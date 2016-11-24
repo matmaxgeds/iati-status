110445d89f230cc71bfd970845e0e63c13eeaff0
dalepotter <dalepotter@gmail.com>
2016-11-24 17:05:08 +0000

    Add tests for the IATI Datastore This adds a 200 response and link checks for the IATI Datastore

---
+++ tests/test_datastore.py
@@ -0,0 +1,15 @@
+import pytest
+from web_test_base import *
+
+class TestIATIDatastore(WebTestBase):
+    urls_to_get = [
+        "http://datastore.iatistandard.org/"
+    ]
+
+    def test_contains_links(self, loaded_request):
+        """
+        Test that each page contains links to the defined URLs.
+        """
+        result = self._get_links_from_page(loaded_request)
+
+        assert "http://iatiregistry.org/" in result
