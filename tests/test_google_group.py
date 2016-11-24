b023316bfa98a7b864044fe1274dbaed52990d77
Hayden <hayden.field@devinit.org>
2016-11-24 15:50:33 +0000

    Add a test for the IATI Google Group
    This tests the 200 response.
    It would ideally be checked that there is a link to IATI Discuss.
    This is not done because of the way the page loads.
    This functionality could be added later with Selenium.

---
+++ tests/test_google_group.py
@@ -0,0 +1,18 @@
+import pytest
+from web_test_base import *
+
+class TestGoogleGroup(WebTestBase):
+    urls_to_get = [
+        "https://groups.google.com/forum/#!forum/iati-technical"
+    ]
+
+    def test_locate_links(self, loaded_request):
+        """
+        Tests that a page contains links to the defined URLs.
+        This test would ideally check to see whether there is a link to:
+            http://discuss.iatistandard.org
+        Google Groups, however, is loaded primarily with javascript.
+        As such, the link does not exist upon a simple load of the page.
+        This functionality could be added later with aSelenium test.
+        """
+        pass
