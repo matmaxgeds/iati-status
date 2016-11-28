ef3ba7c762d26720c170300d086c9e5476abf6b0
Hayden <hayden.field@devinit.org>
2016-11-28 15:17:38 +0000

    Add XML validation tests to Validator
    This adds a test to check that the validator detects when pasted
    XML is each valid and invalid.

--- tests/test_test.py
+++ tests/test_test.py
@@ -17,16 +17,6 @@ class TestTest(WebTestBase):
             'url': 'http://iatistandard.org/202/namespaces-extensions/'
             , 'method': 'GET'
         }
-        , 'basic POST': {
-            'url': 'http://validator.iatistandard.org/index.php'
-            , 'method': 'POST'
-            , 'data': {'paste': utility.load_file_contents('invalid.xml')}
-        }
-        , 'duplicated URL': {
-            'url': 'http://validator.iatistandard.org/index.php'
-            , 'method': 'POST'
-            , 'data': {'paste': utility.load_file_contents('valid.xml')}
-        }
     }
 
     @pytest.mark.parametrize("target_request", ["implicit GET"])
@@ -37,32 +27,6 @@ class TestTest(WebTestBase):
         req = self.loaded_request_from_test_name(target_request)
         text_to_find = "technical publishing framework"
         xpath_to_locate = '//*[@id="home-strapline"]/h1'
-
-        result = utility.get_text_from_xpath(req, xpath_to_locate)
-
-        assert utility.substring_in_list(text_to_find, result)
-
-    @pytest.mark.parametrize("target_request", ["basic POST"])
-    def test_invalid_xml(self, target_request):
-        """
-        Tests that each page contains the specified text at the required location.
-        """
-        req = self.loaded_request_from_test_name(target_request)
-        text_to_find = "This is not a well-formed xml file"
-        xpath_to_locate = '//*[@id="status"]/div[2]'
-
-        result = utility.get_text_from_xpath(req, xpath_to_locate)
-
-        assert utility.substring_in_list(text_to_find, result)
-
-    @pytest.mark.parametrize("target_request", ["duplicated URL"])
-    def test_duplicated_url(self, target_request):
-        """
-        Tests that each page contains the specified text at the required location.
-        """
-        req = self.loaded_request_from_test_name(target_request)
-        text_to_find = "This is a well formed xml file."
-        xpath_to_locate = '//*[@id="status"]/div[2]'
 
         result = utility.get_text_from_xpath(req, xpath_to_locate)
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -6,6 +6,20 @@ class TestIATIValidator(WebTestBase):
         'IATI Validator': {
             'url': 'http://validator.iatistandard.org/'
         }
+        , 'Valid paste data': {
+            'url': 'http://validator.iatistandard.org/index.php'
+            , 'method': 'POST'
+            , 'data': {
+                'paste': utility.load_file_contents('valid.xml')
+            }
+        }
+        , 'Invalid paste data': {
+            'url': 'http://validator.iatistandard.org/index.php'
+            , 'method': 'POST'
+            , 'data': {
+                'paste': utility.load_file_contents('invalid.xml')
+            }
+        }
     }
 
     def test_contains_links(self, loaded_request):
@@ -16,10 +30,39 @@ class TestIATIValidator(WebTestBase):
 
         assert "http://iatistandard.org/" in result
 
-    def test_contains_form(self, loaded_request):
+    @pytest.mark.parametrize("target_request", ["IATI Validator"])
+    def test_contains_form(self, target_request):
         """
         Test that the validator contains a form on each of three tabs.
         """
-        assert len(utility.locate_xpath_result(loaded_request, '//*[@id="status"]/div/form')) == 1
-        assert len(utility.locate_xpath_result(loaded_request, '//*[@id="fileTab"]/div/form')) == 1
-        assert len(utility.locate_xpath_result(loaded_request, '//*[@id="extra"]/div/form')) == 1
+        req = self.loaded_request_from_test_name(target_request)
+
+        assert len(utility.locate_xpath_result(req, '//*[@id="status"]/div/form')) == 1
+        assert len(utility.locate_xpath_result(req, '//*[@id="fileTab"]/div/form')) == 1
+        assert len(utility.locate_xpath_result(req, '//*[@id="extra"]/div/form')) == 1
+
+    @pytest.mark.parametrize("target_request", ["Valid paste data"])
+    def test_valid_pasted_xml(self, target_request):
+        """
+        Tests that the validator correctly detects valid XML as valid.
+        """
+        req = self.loaded_request_from_test_name(target_request)
+        text_to_find = "This is a well formed xml file."
+        xpath_to_locate = '//*[@id="status"]/div[2]'
+
+        result = utility.get_text_from_xpath(req, xpath_to_locate)
+
+        assert utility.substring_in_list(text_to_find, result)
+
+    @pytest.mark.parametrize("target_request", ["Invalid paste data"])
+    def test_invalid_pasted_xml(self, target_request):
+        """
+        Tests that the validator correctly detects invalid XML as invalid.
+        """
+        req = self.loaded_request_from_test_name(target_request)
+        text_to_find = "This is not a well-formed xml file"
+        xpath_to_locate = '//*[@id="status"]/div[2]'
+
+        result = utility.get_text_from_xpath(req, xpath_to_locate)
+
+        assert utility.substring_in_list(text_to_find, result)
