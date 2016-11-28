62ebf7c18bc5f29e87859fc551cf705ecb9c015c
Hayden <hayden.field@devinit.org>
2016-11-28 09:28:59 +0000

    Remove commented out code

--- tests/web_test_base.py
+++ tests/web_test_base.py
@@ -14,40 +14,6 @@ class WebTestBase:
     """
     loaded_requests = dict()
 
-    # def _locate_xpath_result(self, request, xpath):
-    #     """
-    #     Takes a Request object and an xpath.
-    #     Locates all instances of the specified xpath content within the html
-    #     associated with the request.
-    #     Returns a list of all the content matching the xpath
-    #     """
-    #     parser = etree.HTMLParser()
-    #     tree = etree.fromstring(request.text, parser)
-    #     return tree.xpath(xpath)
-
-    # def _get_links_from_page(self, request):
-    #     """
-    #     Locates the location of all <a href="...">...</a> tags on the page
-    #     associated with the provided request.
-    #     Returns a list of strings containing the linked URLs
-    #         ie. the contents of the `href` attribute
-    #     """
-    #     return utility.locate_xpath_result(request, "//a[@href]/@href")
-    #
-    # def _get_text_from_xpath(self, request, xpath):
-    #     """
-    #     Locates the notes within the HTML at the specific xpath.
-    #     Returns a list of strings containing the contents of these nodes.
-    #     """
-    #     return utility.locate_xpath_result(request, xpath + "/text()")
-    #
-    # def _substring_in_list(self, substr_to_find, list_to_search):
-    #     """
-    #     Returns a boolean value to indicate whether or not a given substring
-    #     is located within the strings of a list.
-    #     """
-    #     return len([s for s in list_to_search if substr_to_find in s]) > 0
-
     def _loaded_request_from_url(self, url):
         """
         Returns the loaded request for a given URL.
