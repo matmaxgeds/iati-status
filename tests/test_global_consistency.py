94dd2eb6d9b8f65fdb7261f9d5bcff6f1b225840
Hayden <hayden.field@devinit.org>
2016-11-29 15:35:12 +0000

    Add test for activity count consistency
    This checks that the activity counts are consistent between the
    dashboard and datastore API.

--- tests/test_global_consistency.py
+++ tests/test_global_consistency.py
@@ -21,26 +21,61 @@ class TestGlobalConsistency(WebTestBase):
         }
     }
 
+    def test_activity_count_consistency(self):
+        """
+        Test to ensure the activity count is consistent across various
+        locations that display this data.
+        """
+        dash_home_req = self.loaded_request_from_test_name('IATI Dashboard - Homepage')
+        dash_activities_req = self.loaded_request_from_test_name('IATI Dashboard - Activities Page')
+        datastore_api_req = self.loaded_request_from_test_name('Datastore API - Activity Count')
+        dash_home_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[1]/td[1]/a'
+        dash_home_unique_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[2]/td[1]/a'
+        dash_activities_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[1]/div/div[1]/h3/span[1]'
+        dash_activities_unique_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[2]/div/div[1]/h3/span[1]'
+        datastore_api_xpath = '//result/iati-activities/query/total-count'
+        min_activity_count = 550000
+        max_datastore_disparity = 0.1
+
+        dash_home_act_count = utility.get_single_int_from_xpath(dash_home_req, dash_home_xpath)
+        dash_home_unique_act_count = utility.get_single_int_from_xpath(dash_home_req, dash_home_unique_xpath)
+        dash_activities_act_count = utility.get_single_int_from_xpath(dash_activities_req, dash_activities_xpath)
+        dash_activities_unique_act_count = utility.get_single_int_from_xpath(dash_activities_req, dash_activities_unique_xpath)
+        datastore_api_act_count = utility.get_single_int_from_xpath(datastore_api_req, datastore_api_xpath)
+
+        assert dash_home_act_count >= min_activity_count
+        assert dash_home_unique_act_count >= min_activity_count
+        assert dash_activities_act_count >= min_activity_count
+        assert dash_activities_unique_act_count >= min_activity_count
+        assert datastore_api_act_count >= min_activity_count
+
+        assert dash_home_act_count == dash_activities_act_count
+        assert dash_home_unique_act_count == dash_activities_unique_act_count
+        assert dash_home_act_count >= dash_home_unique_act_count
+        assert dash_activities_act_count >= dash_activities_unique_act_count
+
+        assert (datastore_api_act_count >= dash_home_unique_act_count * (1 - max_datastore_disparity)) and (datastore_api_act_count <= dash_home_unique_act_count * (1 + max_datastore_disparity))
+
     def test_publisher_count_consistency(self):
         """
         Test to ensure the publisher count is consistent across various
         locations that display this data.
         """
-        registry_homepage_req = self.loaded_request_from_test_name('IATI Registry')
-        dashboard_homepage_req = self.loaded_request_from_test_name('IATI Dashboard - Homepage')
-        dashboard_publishers_req = self.loaded_request_from_test_name('IATI Dashboard - Publisher Page')
+        registry_home_req = self.loaded_request_from_test_name('IATI Registry')
+        dash_home_req = self.loaded_request_from_test_name('IATI Dashboard - Homepage')
+        dash_publishers_req = self.loaded_request_from_test_name('IATI Dashboard - Publisher Page')
         registry_xpath = '//*[@id="home-icons"]/div/div[2]/div/a/strong'
-        dashboard_home_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[1]/a'
-        dashboard_pub_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[1]/div/div[1]/h3/span[1]'
+        dash_home_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[1]/a'
+        dash_pub_xpath = '//*[@id="wrap"]/div[2]/div[2]/div[1]/div/div[1]/h3/span[1]'
         min_publisher_count = 480
         max_registry_disparity = 0.01
 
-        registry_pub_count = utility.get_single_int_from_xpath(registry_homepage_req, registry_xpath)
-        dashboard_home_pub_count = utility.get_single_int_from_xpath(dashboard_homepage_req, dashboard_home_xpath)
-        dashboard_pub_count = utility.get_single_int_from_xpath(dashboard_publishers_req, dashboard_pub_xpath)
+        registry_pub_count = utility.get_single_int_from_xpath(registry_home_req, registry_xpath)
+        dash_home_pub_count = utility.get_single_int_from_xpath(dash_home_req, dash_home_xpath)
+        dash_pub_count = utility.get_single_int_from_xpath(dash_publishers_req, dash_pub_xpath)
 
         assert registry_pub_count >= min_publisher_count
-        assert dashboard_home_pub_count >= min_publisher_count
-        assert dashboard_pub_count >= min_publisher_count
-        assert dashboard_home_pub_count == dashboard_pub_count
-        assert (registry_pub_count >= dashboard_pub_count * (1 - max_registry_disparity)) and (registry_pub_count <= dashboard_pub_count * (1 + max_registry_disparity))
+        assert dash_home_pub_count >= min_publisher_count
+        assert dash_pub_count >= min_publisher_count
+        assert dash_home_pub_count == dash_pub_count
+        assert (registry_pub_count >= dash_pub_count * (1 - max_registry_disparity)) and (registry_pub_count <= dash_pub_count * (1 + max_registry_disparity))
