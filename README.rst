8dfd58c15cd9cb97d4da39826223d5591505f870
dalepotter <dalepotter@gmail.com>
2016-11-30 16:16:16 +0000

    Add disk space test for backup server Uses SSH and the paramiko library to tests that there is sufficient space available for future backups.

--- README.rst
+++ README.rst
@@ -23,7 +23,7 @@ Technology Overview
 
 Tests are written using `pytest <http://doc.pytest.org>`_.
 
-Most tests are designed to obtain HTML and associated content from IATI websites, using the `requests library <http://docs.python-requests.org>`_.
+Most tests are designed to obtain HTML and associated content from IATI websites, using the `requests library <http://docs.python-requests.org>`_. Additional tests on the status of backups are made using the `paramiko module <http://www.paramiko.org>`_, which checks the status of expected backup files via an SSH connection.
 
 These tests are run daily (at around 12noon) using Travis `cron jobs <https://docs.travis-ci.com/user/cron-jobs/>`_.
 
@@ -44,6 +44,12 @@ Installation
 
 	# Install requirements
 	pip install -r requirements.txt
+
+	# Login credentials for the IATI backup server are stored as environment variables.
+	# Add these lines to the end of your virtualenv set-up script - i.e. pyenv/bin/activate
+	export backup_server_hostname='[YOUR_HOSTNAME]'
+	export backup_server_username='[YOUR_USERNAME]'
+	export backup_server_password='[YOUR_PASSWORD]'
 
 	# Run the tests
 	py.test tests/
--- tests/test_backup_server.py
+++ tests/test_backup_server.py
@@ -1,7 +1,33 @@
 import os
 import pytest
+import paramiko
+from paramiko.ssh_exception import AuthenticationException, SSHException, BadHostKeyException
+import re
 
-class TestIATIBackupServer():
+class TestIATIBackupServer:
+
+    @classmethod
+    def setup_class(self):
+        """
+        Create a SSH client for use by test methods.
+        Login credentials are stored as environment variables.
+        """
+        hostname = os.environ["backup_server_hostname"]
+        username = os.environ["backup_server_username"]
+        password = os.environ["backup_server_password"]
+
+        try:
+            self.client = paramiko.SSHClient()
+            self.client.load_system_host_keys()
+            self.client.connect(hostname, username=username, password=password, timeout=5)
+        except AuthenticationException:
+            print("Authentication failed, please verify your credentials: %s")
+        except SSHException as sshException:
+            print("Unable to establish SSH connection: %s" % sshException)
+        except BadHostKeyException as badHostKeyException:
+            print("Unable to verify server's host key: %s" % badHostKeyException)
+        except Exception as e:
+            print("Operation error: %s" % e)
 
     def test_ping(self):
         """
@@ -13,3 +39,15 @@ class TestIATIBackupServer():
         result = os.system("ping -c 1 {}".format(server_address))
 
         assert result == 0  # 0 indicates the network is active
+
+    def test_at_least_1GB_disk_space_available(self):
+        """
+        Test that there is sufficient space available for future backups.
+        Filters the output of 'df' to return the KBs available to the server's main disk.
+        """
+
+        stdin, stdout, stderr = self.client.exec_command("df /dev/xvda1 | awk '{print $4}' | sed -n 2p | xargs echo -n")
+
+        disk_space_available = int(stdout.read())
+
+        assert disk_space_available > 1000000  # 1000000KB ~ 1GB
