28f8e3a99698c0ecd1605ad62fec3266c55096fd
dalepotter <dalepotter@gmail.com>
2016-12-01 13:04:09 +0000

    Add test for github backups Checks that Github backups have been made in the expected way. Also adds method to return the contents of a given directory

--- tests/test_backup_server.py
+++ tests/test_backup_server.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 import os
 import pytest
 import paramiko
@@ -43,12 +44,71 @@ class TestIATIBackupServer:
 
         assert result == 0  # 0 indicates the network is active
 
+    def _get_directory_contents(self, dir_path):
+        """
+        Returns a dictionary of files/directories present within a given directory
+        path.
+
+        Input:
+          dir_path -- Path to to directory
+
+        Returns:
+          Dictionary with file/directory name as key, then dictionary containing
+          filesizes (in bytes) and last modified time as a datetime object.
+
+          Returns None if no file/directory present.
+        """
+        stdin, stdout, stderr = self.client.exec_command(
+            "find {} -maxdepth 1 -print0 | xargs -0 stat -c '%y %s %n' | perl -pe 'chomp if eof'".format(dir_path)
+            )
+        stdout_unicode = stdout.read().decode('utf-8')
+        stdout_lines = stdout_unicode.split("\n")
+
+        if len(stdout_lines) > 0:
+            output = dict()
+            for line in stdout_lines:
+                line_parts = line.split(" ")
+                relative_path = line_parts[4].replace(dir_path, ".")
+                output[relative_path] = {
+                    "last_modified": datetime.strptime(
+                        "{} {} {}".format(line_parts[0], line_parts[1][0:15], line_parts[2]),
+                        '%Y-%m-%d %H:%M:%S.%f %z'
+                        ),
+                    "filesize": int(line_parts[3])
+                    }
+            return output
+        else:
+            return None
+
+    def test_github_backups_made(self):
+        """
+        Tests that Github backups have been made in the expected way - i.e.
+        - There are at least 115 files present
+        - Every file is greater than 0 bytes
+        - All files have been modified (i.e. updated) the past day
+        """
+        github_backups = self._get_directory_contents("/home/backups/backup-github/github-backups")
+
+        github_backups_filesizes = [v['filesize'] for k,v in github_backups.items()]
+        github_backups_last_modified_dates =  [v['last_modified'] for k,v in github_backups.items()]
+        smallest_backup_filesize = min(github_backups_filesizes)
+        oldest_last_modified_date = min(github_backups_last_modified_dates)
+        datetime_yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+
+        assert len(github_backups) >= 115
+        assert smallest_backup_filesize > 0
+        #TODO Check behaviour of backup script, so that following test is reliable
+        # assert oldest_last_modified_date > datetime_yesterday
+
     def test_csv2iati_backup_made_daily(self):
         """
         Tests that a csv2iati backups has been made within the past 24 hours, and
         that this file has:
         - a filesize greater than 0 bytes
         - a filename matching a regex pattern (which defines the expected filename)
+
+        #TODO Possibly refactor (if appropriate, so that this test uses
+            self._get_directory_contents
         """
         stdin, stdout, stderr = self.client.exec_command(
             "find /home/backups/csv2iati -maxdepth 1 -type f -mtime -1 -print0 | xargs -0 du -b | xargs echo -n"
