f60f16eada38e21bccff3ab78c705abfc6bcf1bb
Hayden <hayden.field@devinit.org>
2017-01-03 10:40:18 +0000

    Specify custom min size for d-portal
    Because of the way d-portal is generated, the base response is very small.
    As such, the base limit of 4000 bytes is too large.

--- tests/test_d_portal.py
+++ tests/test_d_portal.py
@@ -5,5 +5,6 @@ class TestDPortal(WebTestBase):
     requests_to_load = {
         'D-Portal Homepage': {
             'url': 'http://d-portal.org/'
+            , 'min_response_size': 1100
         }
     }
