8737845430f031a8090b6a90682faf4ca52ed2af
Hayden <hayden.field@devinit.org>
2017-06-05 14:10:28 +0100

    Remove test expecting CSV2IATI registration form
    Following the deprecation policy, the registration form has been removed

--- tests/test_csv2iati.py
+++ tests/test_csv2iati.py
@@ -33,21 +33,3 @@ class TestCSV2IATI(WebTestBase):
         assert len(forms) == 1
         assert form_action == ['/login']
         assert len(form_inputs) == 3
-
-    @pytest.mark.parametrize("target_request", ["CSV2IATI Homepage"])
-    def test_registration_form_presence(self, target_request):
-        """
-        Test that there is a valid registration form on the CSV2IATI Homepage.
-        """
-        req = self.loaded_request_from_test_name(target_request)
-        form_xpath = '//*[@id="login_register_forms_container"]/fieldset[2]/form'
-        form_action_xpath = '//*[@id="login_register_forms_container"]/fieldset[2]/form/@action'
-        input_xpath = '//*[@id="login_register_forms_container"]/fieldset[2]/form/dl/dd/input'
-
-        forms = utility.locate_xpath_result(req, form_xpath)
-        form_action = utility.locate_xpath_result(req, form_action_xpath)
-        form_inputs = utility.locate_xpath_result(req, input_xpath)
-
-        assert len(forms) == 1
-        assert form_action == ['/register']
-        assert len(form_inputs) == 5
