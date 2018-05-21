c3762443859ada75687e5a62d576fe8140a42a7c
allthatilk <allthatilk@users.noreply.github.com>
2018-05-21 17:06:27 +0100

    Remove redundant csv2iati test now site has been decommissioned

--- tests/test_csv2iati.py
+++ tests/test_csv2iati.py
@@ -7,29 +7,3 @@ class TestCSV2IATI(WebTestBase):
             'url': 'http://csv2iati.iatistandard.org/'
         }
     }
-
-    def test_contains_links(self, loaded_request):
-        """
-        Test that each page contains links to the defined URLs.
-        """
-        result = utility.get_links_from_page(loaded_request)
-
-        assert "http://iatistandard.org" in result
-
-    @pytest.mark.parametrize("target_request", ["CSV2IATI Homepage"])
-    def test_login_form_presence(self, target_request):
-        """
-        Test that there is a valid login form on the CSV2IATI Homepage.
-        """
-        req = self.loaded_request_from_test_name(target_request)
-        form_xpath = '//*[@id="login_register_forms_container"]/fieldset[1]/form'
-        form_action_xpath = '//*[@id="login_register_forms_container"]/fieldset[1]/form/@action'
-        input_xpath = '//*[@id="login_register_forms_container"]/fieldset[1]/form/dl/dd/input'
-
-        forms = utility.locate_xpath_result(req, form_xpath)
-        form_action = utility.locate_xpath_result(req, form_action_xpath)
-        form_inputs = utility.locate_xpath_result(req, input_xpath)
-
-        assert len(forms) == 1
-        assert form_action == ['/login']
-        assert len(form_inputs) == 3
