832081abc8234bfcdf47c2e9fced83e9041f08ca
dalepotter <dalepotter@gmail.com>
2018-06-04 15:46:17 +0100

    Adjust Xspaths following Dashboard HTML changes
    Changes were made in IATI/IATI-Dashboard#477. There should also be a seperate PR in future to remove the use of magic strings in XPaths

--- tests/test_global_consistency.py
+++ tests/test_global_consistency.py
@@ -49,23 +49,23 @@ class TestGlobalConsistency(WebTestBase):
 
     @pytest.fixture
     def dash_home_activity_count(cls):
-        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[1]/td[1]/a')
+        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[3]/div[2]/div[1]/div[2]/table/tbody/tr[1]/td[1]/a')
 
     @pytest.fixture
     def dash_home_unique_activity_count(cls):
-        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[2]/td[1]/a')
+        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[3]/div[2]/div[1]/div[2]/table/tbody/tr[2]/td[1]/a')
 
     @pytest.fixture
     def dash_home_activity_file_count(cls):
-        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[4]/td[1]/a')
+        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[3]/div[2]/div[1]/div[2]/table/tbody/tr[4]/td[1]/a')
 
     @pytest.fixture
     def dash_home_org_file_count(cls):
-        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[5]/td[1]/a')
+        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[3]/div[2]/div[1]/div[2]/table/tbody/tr[5]/td[1]/a')
 
     @pytest.fixture
     def dash_home_publisher_count(cls):
-        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[1]/a')
+        return cls._locate_int_on_page('IATI Dashboard - Homepage', '//*[@id="wrap"]/div[3]/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[1]/a')
 
     @pytest.fixture
     def dash_activities_activity_count(cls):
