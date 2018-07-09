8c84bbc10a08c783fed22209402ead4672754f57
dalepotter <dalepotter@gmail.com>
2018-07-09 13:51:49 +0100

    Add basic tests for expected links on main site homepage

---
+++ tests/test_iati_standard.py
@@ -0,0 +1,30 @@
+from web_test_base import *
+
+class TestIATIStandard(WebTestBase):
+    requests_to_load = {
+        'IATI Standard Homepage - no www': {
+            'url': 'http://iatistandard.org'
+        },
+        'IATI Standard Homepage - with www': {
+            'url': 'http://www.iatistandard.org'
+        }
+    }
+
+    def test_contains_links(self, loaded_request):
+        """
+        Test that each page contains links to the defined URLs.
+        """
+        result = utility.get_links_from_page(loaded_request)
+
+        # Selection of header links
+        assert "/en/news/" in result
+        assert "/en/about/" in result
+        assert "/en/iati-standard/" in result
+        assert "/en/using-data/" in result
+
+        # Selection of footer links
+        assert "/en/contact/" in result
+        assert "/en/terms-and-conditions/" in result
+        assert "/en/privacy-policy/" in result
+
+
