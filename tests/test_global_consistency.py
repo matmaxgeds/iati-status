0db7db394d11874e190d48d0d6e4184ce5eb4e72
Ocre42 <john.askew@devinit.org>
2018-10-15 16:25:44 +0100

    replace xfail for skip

--- tests/test_global_consistency.py
+++ tests/test_global_consistency.py
@@ -150,7 +150,7 @@ class TestGlobalConsistency(WebTestBase):
         assert dash_home_activity_count >= dash_home_unique_activity_count
         assert dash_activities_activity_count >= dash_activities_unique_activity_count
 
-    @pytest.mark.xfail
+    @pytest.mark.skip(reason="Data is often wrong due to delays between dashboard regeneration cycles")
     def test_activity_count_consistency_datastore_dashboard(self, datastore_api_activity_count, dash_home_unique_activity_count):
         """
         Test to ensure the activity count is consistent, within a margin of error,
