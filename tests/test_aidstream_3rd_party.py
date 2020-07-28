b5804f6810e2ccc5ba852d2df5558a47c8fb2942
Andy Lulham <a.lulham@gmail.com>
2020-07-29 00:18:05 +0100

    Rename param

--- tests/test_aidstream_3rd_party.py
+++ tests/test_aidstream_3rd_party.py
@@ -11,20 +11,20 @@ class TestAidstreamIsLive(WebTestBase):
         'Aidstream homepage': {
             'url': 'https://aidstream.org/'
         },
-        'Aidstream hosted dataset_1': {
+        'Aidstream hosted dataset 1': {
             'url': 'https://aidstream.org/files/xml/abaseen-activities.xml'
         },
-        'Aidstream hosted dataset_2': {
+        'Aidstream hosted dataset 2': {
             'url': 'https://aidstream.org/files/xml/ageintl-activities.xml'
         },
-        'Aidstream hosted dataset_3': {
+        'Aidstream hosted dataset 3': {
             'url': 'http://aidstream.org/files/xml/tfacmalawi-activities.xml'
         }
     }
 
-    @pytest.mark.parametrize("target_request", ["Aidstream hosted dataset_1",
-                                                "Aidstream hosted dataset_2",
-                                                "Aidstream hosted dataset_3"])
+    @pytest.mark.parametrize("target_request", ["Aidstream hosted dataset 1",
+                                                "Aidstream hosted dataset 2",
+                                                "Aidstream hosted dataset 3"])
     def test_load_aidstream_datasets(self, target_request):
         """Test that aidstream sample datasets are live."""
         req = self.loaded_request_from_test_name(target_request)
